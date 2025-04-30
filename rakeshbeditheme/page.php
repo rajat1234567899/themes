5de6bc9579d081e9c348696e9ea63efd56c18279
Shubh1697 <digitalorra.developer5@gmail.com>
2025-04-30 12:14:36 +0530

    page.php

--- rakeshbeditheme/page.php
+++ rakeshbeditheme/page.php
@@ -1,27 +1,34 @@
 <?php
 /**
  * The template for displaying all pages
+ * 
+ * @package your-theme-name
  */
 
-get_header();
-?>
+get_header(); ?>
 
 <main id="primary" class="site-main">
     <?php
-    while (have_posts()) :
-        the_post();
+    while (have_posts()) : the_post();
         
-        get_template_part('template-parts/content', 'page');
-        
-        // If comments are open or we have at least one comment, load up the comment template
-        if (comments_open() || get_comments_number()) :
-            comments_template();
-        endif;
+        // Essential for Elementor:
+        if (class_exists('Elementor\Plugin') && \Elementor\Plugin::$instance->db->is_built_with_elementor(get_the_ID())) {
+            // Elementor content
+            the_content();
+        } else {
+            // Regular WordPress content
+            get_template_part('template-parts/content', 'page');
+            
+            // If comments are open or we have at least one comment
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+        }
         
     endwhile; // End of the loop.
     ?>
 </main><!-- #primary -->
 
 <?php
-get_sidebar();
+get_sidebar(); // Remove if you don't need sidebar
 get_footer();
--- rakeshbeditheme/single.php
+++ rakeshbeditheme/single.php
@@ -1,37 +1,30 @@
 <?php
-/**
- * The template for displaying all single posts
- */
-
-get_header();
-?>
+get_header(); ?>
 
 <main id="primary" class="site-main">
     <?php
-    while (have_posts()) :
-        the_post();
+    while (have_posts()) : the_post();
         
-        get_template_part('template-parts/content', 'single');
+        if (class_exists('Elementor\Plugin') && \Elementor\Plugin::$instance->db->is_built_with_elementor(get_the_ID())) {
+            the_content();
+        } else {
+            get_template_part('template-parts/content', 'single');
+            
+            // Post navigation
+            the_post_navigation(array(
+                'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'your-theme') . '</span> <span class="nav-title">%title</span>',
+                'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'your-theme') . '</span> <span class="nav-title">%title</span>',
+            ));
+            
+            // Comments
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+        }
         
-        // If comments are open or we have at least one comment, load up the comment template
-        if (comments_open() || get_comments_number()) :
-            comments_template();
-        endif;
-        
-        // Previous/next post navigation
-        the_post_navigation(array(
-            'next_text' => '<span class="meta-nav" aria-hidden="true">' . esc_html__('Next', 'my-custom-theme') . '</span> ' .
-                '<span class="screen-reader-text">' . esc_html__('Next post:', 'my-custom-theme') . '</span> ' .
-                '<span class="post-title">%title</span>',
-            'prev_text' => '<span class="meta-nav" aria-hidden="true">' . esc_html__('Previous', 'my-custom-theme') . '</span> ' .
-                '<span class="screen-reader-text">' . esc_html__('Previous post:', 'my-custom-theme') . '</span> ' .
-                '<span class="post-title">%title</span>',
-        ));
-        
-    endwhile; // End of the loop.
+    endwhile;
     ?>
-</main><!-- #primary -->
+</main>
 
 <?php
-get_sidebar();
 get_footer();
